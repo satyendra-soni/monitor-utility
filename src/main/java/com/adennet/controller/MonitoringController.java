fdbeefeae04335888c0728b1e8e2a58cd0f7ab69
Onkar Pawar <onkipawar@gmail.com>
2023-01-13 06:34:39 +0000

    JBADEN-460:Added spring.profiles.active in jenkins file

--- src/main/java/com/adennet/controller/MonitoringController.java
+++ src/main/java/com/adennet/controller/MonitoringController.java
@@ -4,12 +4,14 @@ import com.adennet.dto.ServerDetail;
 import com.adennet.dto.SessionCount;
 import com.adennet.service.MonitoringService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RestController
 @RequestMapping(("/monitor"))
 @RequiredArgsConstructor
@@ -26,6 +28,7 @@ public class MonitoringController {
 
     @GetMapping("activeSession")
     public SessionCount activeSession(@RequestParam("isValid") boolean isValid) {
+        log.info("Retrieving active session from radius sessions.");
         return monitoringService.activeSession(isValid);
     }
 
