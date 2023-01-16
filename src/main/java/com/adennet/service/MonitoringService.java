4e2836bf4e0f31fd01395c4bf4e78c8dc99aa8ab
satyendra-soni <satyendra.soni@appdirect.com>
2023-01-16 23:42:02 +0530

    added methods

--- src/main/java/com/adennet/service/MonitoringService.java
+++ src/main/java/com/adennet/service/MonitoringService.java
@@ -1,46 +1,30 @@
 package com.adennet.service;
 
-import com.adennet.dto.ConsumptionUsages;
-import com.adennet.dto.DbResponse;
-import com.adennet.dto.ServerDetail;
-import com.adennet.dto.SessionCount;
+import com.adennet.dto.*;
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.data.mongodb.MongoExpression;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
-import org.springframework.data.mongodb.core.aggregation.AggregationExpression;
-import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 import static com.adennet.util.AppUtil.getDiskDetails;
-import static com.adennet.util.AppUtil.parseDateTime;
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class MonitoringService {
-    private final MongoTemplate radiusDbMongoClient;
-    private final MongoTemplate mediationDbMongoClient;
-    private final JdbcTemplate usageMgmtDbClient;
 
-
-    @Autowired
-    public MonitoringService(@Qualifier("radiusDbMongoClient") MongoTemplate radiusDbMongoClient,
-                             @Qualifier("mediationDbMongoClient") MongoTemplate mediationDbMongoClient,
-                             @Qualifier("usageMgmtDbClient") JdbcTemplate usageMgmtDbClient) {
-        this.radiusDbMongoClient = radiusDbMongoClient;
-        this.mediationDbMongoClient = mediationDbMongoClient;
-        this.usageMgmtDbClient = usageMgmtDbClient;
-    }
+    private final MonitorHelperService  monitorHelperService;
 
     @SneakyThrows
     public Map<String, String> diskDetails(String server, ServerDetail serverDetail) {
@@ -62,40 +46,48 @@ public class MonitoringService {
                 match(Criteria.where("isValid").is(isValid)),
                 group("$null").count().as("count")
         );
-        return radiusDbMongoClient.aggregate(agg, "radius_session", SessionCount.class).getUniqueMappedResult();
+        return monitorHelperService.getRadiusDbMongoClient().aggregate(agg, "radius_session", SessionCount.class).getUniqueMappedResult();
+    }
+    @Async()
+    public void performDataMatching() {
+        long start = System.currentTimeMillis();
+        List<ConsumptionUsages> consumptionUsages = monitorHelperService.getConsumptionDataFromPostgres();
+        System.out.println("consumptionUsages = " + consumptionUsages);
+        System.out.println("consumptionUsages size = " + consumptionUsages.size());
+
+        List<MatchResult> matchResultList = consumptionUsages.stream()
+                .map(usage -> {
+                    CompletableFuture<CumulativeDataUsage> sessionData = monitorHelperService.getSessionData(usage.getSubscriberNumber(), usage.getStartDate(), usage.getEndDate());
+                    CompletableFuture<QuantitySum> bhmrData = monitorHelperService.getBhmrData(usage.getId());
+                    CompletableFuture.allOf(sessionData, bhmrData).join();
+                    CumulativeDataUsage cumulativeDataUsage;
+                    QuantitySum quantitySum;
+                    try {
+                        cumulativeDataUsage = sessionData.get();
+                        quantitySum = bhmrData.get();
+                    } catch (InterruptedException | ExecutionException e) {
+                        throw new RuntimeException(e);
+                    }
+                    if (cumulativeDataUsage != null && quantitySum != null) {
+                        double usedQuantityInConsumptionUsagesMap = usage.getInitialQuantity() - usage.getAvailableQuantity();
+                        System.out.println("SubscriberNumber="+ usage.getSubscriberNumber()+" usedQuantityInConsumptionUsagesMap = " + usedQuantityInConsumptionUsagesMap+" sessionData="
+                                +cumulativeDataUsage.getSumOfCumulativeDataUsage()+"  bhmrData="+quantitySum.getSumOfQuantity());
+                        if (usedQuantityInConsumptionUsagesMap == cumulativeDataUsage.getSumOfCumulativeDataUsage() && usedQuantityInConsumptionUsagesMap == quantitySum.getSumOfQuantity()) {
+                            return MatchResult.builder()
+                                    .subscriberNumber(usage.getSubscriberNumber())
+                                    .usedQuantityInConsumptionUsagesMap(usedQuantityInConsumptionUsagesMap)
+                                    .usedQuantityInRadiusSession(cumulativeDataUsage.getSumOfCumulativeDataUsage())
+                                    .usedQuantityInMediationRecord(quantitySum.getSumOfQuantity())
+                                    .build();
+                        }
+                    }
+                    return null;
+                })
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+        System.out.println("matchResultList = " + matchResultList);
+        long totalTime = System.currentTimeMillis() - start;
+        System.out.println("System.currentTimeMillis() = " + totalTime);
     }
 
-    public void getConsumptionDataFromPostgres() {
-        String sql = "SELECT subscriber_number,start_date,end_date,initial_quantity,available_quantity from consumption_usage_map WHERE status='ACTIVE'";
-        List<ConsumptionUsages> usages = usageMgmtDbClient.query(
-                sql,
-                (rs, rowNum) ->
-                        ConsumptionUsages.builder()
-                                .subscriberNumber(rs.getString("subscriber_number"))
-                                .startDate(rs.getString("start_date"))
-                                .endDate(rs.getString("end_date"))
-                                .initialQuantity(rs.getString("initial_quantity"))
-                                .availableQuantity(rs.getString("available_quantity"))
-                                .build()
-        );
-        System.out.println("usages = " + usages);
-    }
-
-
-    public DbResponse getSessionData(String subscriberNumber, String startDate, String endDate) {
-        log.info("subscriberNumber ={},startDate={},endDate={} ", subscriberNumber, startDate, endDate);
-        Query query = new Query();
-        query.addCriteria(Criteria.where("subscriberNumber").is(subscriberNumber).and("createdAt").gt(parseDateTime(startDate)).lt(parseDateTime(endDate)));
-        long count = radiusDbMongoClient.count(query, "radius_session");
-        System.out.println("count== = " + count);
-        Aggregation agg = newAggregation(
-                match(Criteria.where("subscriberNumber").is(subscriberNumber).and("createdAt").gt(parseDateTime(startDate)).lt(parseDateTime(endDate))),
-                group("$null").sum(AggregationExpression.from(MongoExpression.create("$toDouble:?0", "$cumulativeDataUsage"))).as("sum_cumulativeDataUsage"),
-                project("sum_cumulativeDataUsage")
-
-        );
-        AggregationResults<DbResponse> radiusSession = radiusDbMongoClient.aggregate(agg, "radius_session", DbResponse.class);
-        System.out.println("radiusSession.getUniqueMappedResult() = " + radiusSession.getUniqueMappedResult());
-        return radiusSession.getUniqueMappedResult();
-    }
 }
